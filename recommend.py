13cf37b89a028ba87fe888c6e21b2cd2ba82e700
Niema Moshiri <niemamoshiri@gmail.com>
2025-11-15 09:40:21 -0800

    Finished prototype

--- recommend.py
+++ recommend.py
@@ -10,12 +10,12 @@ from gzip import open as gopen
 from json import dump as jdump
 from niemarkov import MarkovChain, random_choice
 from pathlib import Path
-from pandas import read_csv
-from random import choice
+from random import choice, shuffle
 import argparse
 
 # constants
 DEFAULT_BUFSIZE = 1048576 # 1 MB
+DEFAULT_NUM_STEPS = 1000
 
 # parse + check user args
 def parse_args():
@@ -30,6 +30,7 @@ def parse_args():
     parser.add_argument('-n', '--num_recs', required=True, type=int, help="Number of Recommendations")
     parser.add_argument('-o', '--output', required=True, type=str, help="Output Recommendations File (JSON)")
     parser.add_argument('--no_pseudocount', action="store_true", help="Don't Add Pseudocounts")
+    parser.add_argument('--num_steps', required=False, type=int, default=DEFAULT_NUM_STEPS, help="Maximum Number of Steps in Random Walk")
     args = parser.parse_args()
 
     # check args for validity and return
@@ -107,12 +108,25 @@ def load_item_details(p, column_item):
     # load data
     delim = Sniffer().sniff(f.read(DEFAULT_BUFSIZE)).delimiter
     f.seek(0)
-    df = read_csv(f, delimiter=delim)
+    data = dict() # data[item][attribute] = value
+    for row_num, row in enumerate(reader(f, delimiter=delim)):
+        row_stripped = [s.strip() for s in row]
+        if row_num == 0:
+            header = row_stripped
+            col2ind = {col:ind for ind, col in enumerate(row_stripped)}
+            ind_item = col2ind[column_item]
+            del col2ind[column_item]
+        else:
+            data[row_stripped[ind_item]] = {col:row_stripped[ind] for col, ind in col2ind.items()}
     f.close()
-    return df
+    return data
 
 # produce recommendations for all users
-def recommend(mc, data, item_details, num_recs):
+def recommend(mc, data, item_details, num_recs, num_steps=DEFAULT_NUM_STEPS):
+    # make sure number of recommendations is at most the total number of items
+    all_items = set(item_details.keys())
+    num_recs = min(num_recs, len(all_items))
+
     # create initial recommendations by Markov chain random walk
     recs = dict()
     for user, inspections in data.items():
@@ -134,13 +148,23 @@ def recommend(mc, data, item_details, num_recs):
 
         # get recommendations from random walk
         curr_recs_set = set(); curr_recs_list = list()
-        for curr_node in mc.random_walk(final_node, start_is_node=True):
+        for curr_node in mc.random_walk(final_node, start_is_node=True, num_steps=num_steps):
             if len(curr_recs_set) == num_recs:
                 break
             next_item = mc.labels[curr_node[-1]]
             if next_item not in curr_recs_set:
                 curr_recs_set.add(next_item)
                 curr_recs_list.append(next_item)
+
+        # if too few recommendations, fill them randomly (currently uniformly, maybe more sophisticated later)
+        if len(curr_recs_list) < num_recs:
+            remaining_items = list(all_items)
+            shuffle(remaining_items)
+            while len(curr_recs_list) < num_recs:
+                next_item = remaining_items.pop()
+                if next_item not in curr_recs_set:
+                    curr_recs_set.add(next_item)
+                    curr_recs_list.append(next_item)
         recs[user] = curr_recs_list
     return recs
 
@@ -158,7 +182,7 @@ if __name__ == '__main__':
     data = load_interactions(args.input, args.column_user, args.column_item, args.column_time)
     print("done")
     print("Loading item details from: %s ..." % args.item_details, end=' ')
-    item_details = load_item_details(args.item_details, args.column_item) # TODO
+    item_details = load_item_details(args.item_details, args.column_item)
     print("done")
     print("Producing recommendations...", end=' ')
     recs = recommend(mc, data, item_details, args.num_recs)
